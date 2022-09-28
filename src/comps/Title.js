f960b3bf6c7302470584c5e75e60b03e20f7c27e
Manan Bansal <manan.bansal.1806@gmail.com>
2022-09-28 14:18:57 +0530

    files

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+// import  'bootstrap/dist/css/bootstrap.min.css';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1 className=''>Imagify</h1>
+      <h2>All the Pictures</h2>
+      <p>Publish your imagify to showcase here.. </p>
     </div>
   )
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
-  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
-  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
-  projectId: "the-net-ninja-sandbox",
-  storageBucket: "the-net-ninja-sandbox.appspot.com",
-  messagingSenderId: "485942827092",
-  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
+const firebaseConfig = {
+  apiKey: "AIzaSyDg36mk-Dpd4jSy8pPiw1IpmyATvy-dScc",
+  authDomain: "imagify200.firebaseapp.com",
+  projectId: "imagify200",
+  storageBucket: "imagify200.appspot.com",
+  messagingSenderId: "924637467531",
+  appId: "1:924637467531:web:93e00e193655ae1d69158e"
 };
 
 // Initialize Firebase
